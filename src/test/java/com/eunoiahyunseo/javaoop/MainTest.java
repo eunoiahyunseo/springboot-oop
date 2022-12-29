1374eb25c1c2bcaf282e8b83b96225159500d6f7
eunoiahyunseo <heart20021010@gmail.com>
2022-12-29 18:32:04 +0900

    Implement #1 - Logic, Service (Test)
    testing (TDD)

---
+++ src/test/java/com/eunoiahyunseo/javaoop/MainTest.java
@@ -0,0 +1,21 @@
+package com.eunoiahyunseo.javaoop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author ihyeonseo
+ */
+class MainTest {
+
+    @Test
+    void main() {
+        // Given
+        String[] args = {"3", "1", "2"};
+
+        // When & Then
+        Main.main(args);
+
+    }
+}
