90370c408f51acd45bc5e84ef0ff3bbe03fe67e0
eunoiahyunseo <heart20021010@gmail.com>
2022-12-29 18:31:21 +0900

    Implement #1 - Logic, Service
    develop

---
+++ src/main/java/com/eunoiahyunseo/javaoop/logic/JavaSort.java
@@ -0,0 +1,20 @@
+package com.eunoiahyunseo.javaoop.logic;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author ihyeonseo
+ */
+
+public class JavaSort <T extends Comparable<T>> implements Sort<T> {
+
+    public List<T> sort(List<T> list) {
+        List<T> output = new ArrayList<>(list);
+        Collections.sort(output);
+
+        return output;
+    }
+
+}
